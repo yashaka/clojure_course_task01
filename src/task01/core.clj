ba57a17d1efed60cae950d77a48e1e53dd939966
Iakiv Kramarenko <yashaka@gmail.com>
2013-03-21 01:49:07 +0200

    moved smart-flat to the letfn for better readability

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -4,13 +4,11 @@
 
 
 (defn get-links []
-  (map #(get-in % [2 1 :href])            ;locating h3/a/href inside coll with all h3 with hrefs
-       (filter coll? ((fn smart-flat [x] ;filtering the output of the smart-flat fn applied to the parsed html
-                                         ;  smart-flat smartly flattens everything passed except h3 with {:class "r"}
-                        (if (and (coll? x) (not= (get-in x [1 :class]) "r")) 
-                          (mapcat smart-flat x)
-                          [x]))
-                      (parse "clojure_google.html")))))
+  (map #(get-in % [2 1 :href])                ;locating h3/a/href inside coll with all h3 with hrefs
+       (filter coll? (letfn [(smart-flat [x]  ;smart-flat smartly flattens everything passed except h3 with {:class "r"}
+                               (if (and (coll? x) (not= (get-in x [1 :class]) "r")) 
+                                 (mapcat smart-flat x) [x]))]
+                       (smart-flat (parse "clojure_google.html"))))))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
